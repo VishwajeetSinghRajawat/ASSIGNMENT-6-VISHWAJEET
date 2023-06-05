55d1d60ce2d87424a5b4e1e4e4c72e61213e2a96
VishwajeetSinghRajawat <131696813+VishwajeetSinghRajawat@users.noreply.github.com>
2023-06-05 23:55:48 +0530

    Add files via upload

---
+++ ASSIGNMENT 6 Q5.py
@@ -0,0 +1,5 @@
+# ASSIGNMENT 6 Q5
+n=input("enter the string: ")
+l=n.split('-')
+l.sort()
+print('-'.join(l))
---
+++ ASSIGNMENT 6 Q6.py
@@ -0,0 +1,4 @@
+# ASSIGNMENT-6 Q6
+def student_data(student_id, student_name, student_class):
+    return f'Student ID: {student_id}\nStudent Name: {student_name}\nClass: {student_class}'
+print(student_data('22102102', 'VISHWAJEET SINGH RAJAWAT', 'CIVIl'))
